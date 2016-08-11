415c8821dfc91d5990cb463f31e4b1b4c1ebf32b
Phil Wilt <pwilt@onehub.com>
2016-08-11 15:43:47 -0700

    delte notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -20,7 +20,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} />
+        <Notes notes={notes} onDelete={this.deleteNote} />
       </div>
     );
   }
@@ -31,6 +31,14 @@ export default class App extends React.Component {
         id: uuid.v4(),
         task: 'New task'
       }])
+    });
+  }
+
+  deleteNote = (id, e) => {
+    e.stopPropagation();
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
     });
   }
 }
