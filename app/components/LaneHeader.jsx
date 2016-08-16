3a1dc8dabb41eafd8c19d2242bac0538055d22dd
Phil Wilt <pwilt@onehub.com>
2016-08-15 20:26:08 -0700

    style, lane functionality

--- app/components/LaneHeader.jsx
+++ app/components/LaneHeader.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import uuid from 'uuid'
 import connect from '../libs/connect'
+import Editable from './Editable'
 import NoteActions from '../actions/NoteActions'
 import LaneActions from '../actions/LaneActions'
 
@@ -24,12 +25,37 @@ export default connect(() => ({}), {
     })
   }
 
+  const activateLaneEdit = () => {
+      LaneActions.update({
+        id: lane.id,
+        editing: true
+      })
+    }
+    const editName = name => {
+      LaneActions.update({
+        id: lane.id,
+        name,
+        editing: false
+      });
+    }
+
+    const deleteLane = e => {
+      e.stopPropagation()
+
+      LaneActions.delete(lane.id)
+    }
+
   return (
-    <div className="lane-header">
+    <div className="lane-header" onClick={activateLaneEdit} {...props}>
       <div className="lane-add-note">
         <button onClick={addNote}>+</button>
       </div>
-      <div className="lane-name">{lane.name}</div>
+
+      <Editable className="lane-name" editing={lane.editing} value={lane.name} onEdit={editName} ></Editable>
+
+      <div className="lane-delete">
+        <button onClick={deleteLane}>x</button>
+      </div>
     </div>
   )
 })
