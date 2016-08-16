0044ef1aadbc65115885118a96929c4b07a94c00
Phil Wilt <pwilt@onehub.com>
2016-08-15 20:00:56 -0700

    add lanes

--- app/components/Provider/setup.js
+++ app/components/Provider/setup.js
@@ -1,10 +1,13 @@
 
-import storage from '../../libs/storage';
-import persist from '../../libs/persist';
-import NoteStore from '../../stores/NoteStore';
+import storage from '../../libs/storage'
+import persist from '../../libs/persist'
+import NoteStore from '../../stores/NoteStore'
+import LaneStore from '../../stores/LaneStore'
 
 export default alt => {
-  alt.addStore('NoteStore', NoteStore);
+  alt.addStore('NoteStore', NoteStore)
+
+  alt.addStore('LaneStore', LaneStore)
 
   persist(alt, storage(localStorage), 'app')
 }
